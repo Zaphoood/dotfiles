5adeef4fa4a5f0e2765be2b378aef86f2cfff016
Mathis Weber <mathis.c.weber@web.de>
2022-03-30 17:37:24 +0200

    awesome: Swap Super + j / Super + k behavior

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -260,13 +260,16 @@ globalkeys = gears.table.join(
 
     awful.key({ modkey,           }, "j",
         function ()
-            awful.client.focus.byidx( 1)
+            -- Swap the behavior of j and k - I think it makes more sense this way
+            -- awful.client.focus.byidx( 1)
+            awful.client.focus.byidx( -1)
         end,
         {description = "focus next by index", group = "client"}
     ),
     awful.key({ modkey,           }, "k",
         function ()
-            awful.client.focus.byidx(-1)
+            -- awful.client.focus.byidx(-1)
+            awful.client.focus.byidx( 1)
         end,
         {description = "focus previous by index", group = "client"}
     ),
