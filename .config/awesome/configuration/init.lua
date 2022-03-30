7c4975e082a8d89d6c0743e7f93258b3911ded99
Mathis Weber <mathis.c.weber@web.de>
2022-03-30 22:23:52 +0200

    Modularize keys

---
+++ .config/awesome/configuration/init.lua
@@ -0,0 +1,10 @@
+-- Standard awesome library
+local awful = require("awful")
+local gears = require("gears")
+local gfs = gears.filesystem
+local wibox = require("wibox")
+
+-- Theme handling library
+local beautiful = require("beautiful")
+
+require("configuration.keys")
