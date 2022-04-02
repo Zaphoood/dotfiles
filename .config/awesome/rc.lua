b76d7e6a59340e37d3a5979b6e6afda96e1a7c8a
Mathis Weber <mathis.c.weber@web.de>
2022-04-02 15:46:22 +0200

    awesome: Move beautiful and layouts to ui.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -20,6 +20,15 @@ local panic_mode_key = gears.table.join(
 -- Note that the above key combination will removed by a later root.keys() call
 root.keys(panic_mode_key)
 
+
+-- This is used later as the default terminal and editor to run.
+terminal = "x-terminal-emulator"
+editor = os.getenv("EDITOR") or "editor"
+editor_cmd = terminal .. " -e " .. editor
+
+-- File explorer
+file_explorer = "nautilus"
+
 -- Theme handling library
 local beautiful = require("beautiful")
 -- Notification library
@@ -55,42 +64,6 @@ do
         in_error = false
     end)
 end
--- }}}
-
--- {{{ Variable definitions
--- Themes define colours, icons, font and wallpapers.
--- beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
-beautiful.init("/home/mathis/.config/awesome/themes/main/theme.lua")
-
-
--- This is used later as the default terminal and editor to run.
-terminal = "x-terminal-emulator"
-editor = os.getenv("EDITOR") or "editor"
-editor_cmd = terminal .. " -e " .. editor
-
--- File explorer
-file_explorer = "nautilus"
-
-
--- Table of layouts to cover with awful.layout.inc, order matters.
-awful.layout.layouts = {
-    awful.layout.suit.floating,
-    awful.layout.suit.tile,
-    awful.layout.suit.tile.left,
-    awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
-    -- awful.layout.suit.fair,
-    -- awful.layout.suit.fair.horizontal,
-    -- awful.layout.suit.spiral,
-    -- awful.layout.suit.spiral.dwindle,
-    -- awful.layout.suit.max,
-    -- awful.layout.suit.max.fullscreen,
-    -- awful.layout.suit.magnifier,
-    -- awful.layout.suit.corner.nw,
-    -- awful.layout.suit.corner.ne,
-    -- awful.layout.suit.corner.sw,
-    -- awful.layout.suit.corner.se,
-}
 -- }}}
 
 require("configuration.ui")
