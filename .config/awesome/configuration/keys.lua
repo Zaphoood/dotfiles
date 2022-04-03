81c6a25d7aced961ef014d5abcd7a63a30280f3e
Mathis Weber <mathis.c.weber@web.de>
2022-04-03 18:04:18 +0200

    Fix clientkeys/buttons not being exported

--- .config/awesome/configuration/keys.lua
+++ .config/awesome/configuration/keys.lua
@@ -160,55 +160,58 @@ keys.globalkeys = gears.table.join(
     )
 
 
-    local clientkeys = gears.table.join(
-      awful.key({ modkey,           }, "f",
-          function (c)
-              c.fullscreen = not c.fullscreen
-              c:raise()
-          end,
-          {description = "toggle fullscreen", group = "client"}),
-      awful.key({ modkey, shift   }, "c",      function (c) c:kill()                         end,
-                {description = "close", group = "client"}),
-      -- awful.key({ modkey, ctrl }, "space",  awful.client.floating.toggle                     ,
-      --       {description = "toggle floating", group = "client"}),
-  awful.key({ modkey, ctrl }, "Return", function (c) c:swap(awful.client.getmaster()) end,
+keys.clientkeys = gears.table.join(
+    awful.key({ modkey,           }, "f",
+      function (c)
+          c.fullscreen = not c.fullscreen
+          c:raise()
+      end,
+      {description = "toggle fullscreen", group = "client"}),
+    awful.key({ modkey, shift   }, "c",      function (c) c:kill()                         end,
+            {description = "close", group = "client"}),
+    -- awful.key({ modkey, ctrl }, "space",  awful.client.floating.toggle                     ,
+    --       {description = "toggle floating", group = "client"}),
+    awful.key({ modkey, ctrl }, "Return", function (c) c:swap(awful.client.getmaster()) end,
             {description = "move to master", group = "client"}),
-  -- awful.key({ modkey,           }, "o",      function (c) c:move_to_screen()               end,
-  --           {description = "move to screen", group = "client"}),
-  awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end,
+    -- awful.key({ modkey,           }, "o",      function (c) c:move_to_screen()               end,
+    --           {description = "move to screen", group = "client"}),
+    awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end,
             {description = "toggle keep on top", group = "client"}),
-  awful.key({ modkey,           }, "n",
+    awful.key({ modkey,           }, "n",
       function (c)
           -- The client currently has the input focus, so it cannot be
           -- minimized, since minimized clients can't have the focus.
           c.minimized = true
       end ,
       {description = "minimize", group = "client"}),
-  awful.key({ modkey,           }, "m",
+    awful.key({ modkey,           }, "m",
       function (c)
           c.maximized = not c.maximized
           c:raise()
       end ,
       {description = "(un)maximize", group = "client"}),
-  -- awful.key({ modkey, ctrl }, "m",
-  --     function (c)
-  --         c.maximized_vertical = not c.maximized_vertical
-  --         c:raise()
-  --     end ,
-  --     {description = "(un)maximize vertically", group = "client"}),
-  awful.key({ modkey, shift   }, "m",
+    -- awful.key({ modkey, ctrl }, "m",
+    --     function (c)
+    --         c.maximized_vertical = not c.maximized_vertical
+    --         c:raise()
+    --     end ,
+    --     {description = "(un)maximize vertically", group = "client"}),
+    awful.key({ modkey, shift   }, "m",
       function (c)
           c.maximized_horizontal = not c.maximized_horizontal
           c:raise()
       end ,
       {description = "(un)maximize horizontally", group = "client"}),
 
-  -- My own client keybindings
-  -- Alt + F4: Kill active client 
-  awful.key({ alt }, "F4", function (c) c:kill() end,
+    -- My own client keybindings
+    -- Alt + F4: Kill active client 
+    awful.key({ alt }, "F4", function (c)
+                naughty.notify({ text = "kill" })
+                c:kill()
+            end,
             {description = "kill client", group = "client"}),
-  -- Modkey + W: Kill active client
-  awful.key({ modkey }, "w", function (c) c:kill() end,
+    -- Modkey + W: Kill active client
+    awful.key({ modkey }, "w", function (c) c:kill() end,
             {description = "kill client", group = "client"})
 )
 
@@ -265,7 +268,7 @@ end
 -- }}}
 
 -- {{{ Mouse bindings
-local clientbuttons = gears.table.join(
+keys.clientbuttons = gears.table.join(
   awful.button({ }, 1, function (c)
       c:emit_signal("request::activate", "mouse_click", {raise = true})
   end),
