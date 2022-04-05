abfa2f4da10806e5e275b9bf9bfabdca8221c157
Mathis Weber <mathis.c.weber@web.de>
2022-04-05 18:31:01 +0200

    awesome: Proper interpolation in lock script

--- .config/awesome/script/lock.sh
+++ .config/awesome/script/lock.sh
@@ -1,54 +1,54 @@
 #!/bin/sh
 
 # Using the catppuccin color palette
-B='#302d41ff'  # blank:     Black 3
-C='#c3bac6ff'  # clear:     Gray 2
-D='#988BA2ff'  # typing:    Blue
-T='#96cdfbff'  # text:      Blue
-W='#f28fadff'  # wrong:     Red
-V='#f8bd96ff'  # verifying: Peach
-VT='#96cdfbff' # default:   Gray 1
+c_blank='#302d41ff'  # Black 3
+c_clear='#c3bac6ff'  # Gray 2
+c_ring='#988BA2ff'   # Blue
+c_layout='#96cdfbff' # Blue
+c_wrong='#f28fadff'  # Red
+c_verif='#f8bd96ff'  # Peach
+c_typing='#96cdfbff' # Gray 1
 
 # Formatting
 time_format="%H:%M:%S" 
-date_format="%A %M %B"
+date_format="%A, %M. %B"
 
-i3lock                    \
---radius 60               \
---ind-pos="w/2:h/2"       \
---time-pos="w/2:h/2-200"  \
---date-pos="w/2:h/2-160"  \
---greeter-pos="w/2:h/2"   \
---insidever-color=$B      \
---ringver-color=$V        \
---ring-width=6            \
-                          \
---insidewrong-color=$W    \
---ringwrong-color=$C      \
-                          \
---inside-color=$B         \
---ring-color=$D           \
---line-color=$B           \
---separator-color=$D      \
-                          \
---verif-color=$VT         \
---wrong-color=$C          \
---time-color=$C           \
---date-color=$C           \
---layout-color=$T         \
---keyhl-color=$VT         \
---bshl-color=$W           \
-                          \
---screen 1                \
---blur 16                 \
---clock                   \
---indicator               \
---time-str=$time_format   \
---date-str=$date_format   \
---wrong-text="incorrect!" \
---verif-text="verifying"  \
---time-size=75            \
---date-size=30            \
---verif-size=20           \
---wrong-size=20           \
+i3lock                       \
+--radius 60                  \
+--ind-pos="w/2:h/2"          \
+--time-pos="w/2:h/2-200"     \
+--date-pos="w/2:h/2-160"     \
+--greeter-pos="w/2:h/2"      \
+--insidever-color=$c_blank   \
+--ringver-color=$c_verif     \
+--ring-width=6               \
+                             \
+--insidewrong-color=$c_blank \
+--ringwrong-color=$c_wrong   \
+                             \
+--inside-color=$c_blank      \
+--ring-color=$c_ring         \
+--line-color=$c_blank        \
+--separator-color=$c_ring    \
+                             \
+--verif-color=$c_verif       \
+--wrong-color=$c_clear       \
+--time-color=$c_clear        \
+--date-color=$c_clear        \
+--layout-color=$c_layout     \
+--keyhl-color=$c_typing      \
+--bshl-color=$c_wrong        \
+                             \
+--screen 1                   \
+--blur 16                    \
+--clock                      \
+--indicator                  \
+--time-str="$time_format"    \
+--date-str="$date_format"    \
+--wrong-text="Incorrect!"    \
+--verif-text="Verifying"     \
+--time-size=75               \
+--date-size=30               \
+--verif-size=20              \
+--wrong-size=20              \
 
