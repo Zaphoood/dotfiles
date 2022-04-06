134b4e1570ee23e3a2c37a1152fb256a6ba1ade8
Mathis Weber <mathis.c.weber@web.de>
2022-04-06 22:52:52 +0200

    awesome: Extend margin-wrapping helper function

--- .config/awesome/configuration/init.lua
+++ .config/awesome/configuration/init.lua
@@ -6,6 +6,7 @@ editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 -- }}}
 
+-- {{{ Programs
 -- Command for setting volume
 -- The `-D pulse` flag is necessary when using pulseaudio
 volume_control = "amixer -D pulse"
@@ -15,12 +16,11 @@ volume_change = 5
 -- Command used to control Spotify playback
 -- Must be concatenated with either `Play`, `Pause`, `PlayPause`, `Next` or `Previous`
 spotify_control = "dbus-send --type=method_call --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player."
-
 -- File explorer
 file_explorer = terminal .. " -e ranger"
-
 -- Lock screen
 lock_screen = os.getenv("HOME") .. "/.config/awesome/script/lock.sh"
+-- }}}
 
 -- Keyboard layout and shortcuts
 configuration.keys = require("configuration.keys")
--- .config/awesome/helpers.lua
+++ .config/awesome/helpers.lua
@@ -40,11 +40,44 @@ end
 -- }}}
 
 -- {{{ UI
-function helpers.wrap_margin(widget, margin)
+function helpers.wrap_margin(...)
+    -- Wrap any given widget inside a wibox.widget.margin
+    -- First argument must be the widget, the following arguments can be either
+    -- a number, which will be margin applied to all sides,
+    -- four numbers, which will be the the left, right, top and bottom margins respectively,
+    -- or a table containing the keys `left`, `right`, `top` and/or `bottom`.
+    
+    local n_args = select("#", ...)
+    local widget = select(1, ...)
+    margins = {
+        left   = 0,
+        right  = 0,
+        top    = 0,
+        bottom = 0,
+    }
+    if n_args == 2 then
+        if type(select(2, ...)) == "number" then
+            margins["left"]   = select(2, ...)
+            margins["right"]  = select(2, ...)
+            margins["top"]    = select(2, ...)
+            margins["bottom"] = select(2, ...)
+        elseif type(select(2, ...)) == "table" then
+            margins = select(2, ...)
+        end
+    elseif n_args == 5 then
+        margins["left"]   = select(2, ...)
+        margins["right"]  = select(3, ...)
+        margins["top"]    = select(4, ...)
+        margins["bottom"] = select(5, ...)
+    end
     return wibox.widget {
         { widget = widget },
-        margins = margin,
-        widget = wibox.container.margin }
+        left   = margins["left"],
+        right  = margins["right"],
+        top    = margins["top"],
+        bottom = margins["bottom"],
+        widget = wibox.container.margin,
+    }
 end
 -- }}}
 
