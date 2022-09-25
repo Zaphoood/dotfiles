3ee62bf4f2f1276dbd62dedc8b62ca8ed37db59d
Mathis Weber <mathis.c.weber@web.de>
2022-09-26 00:24:33 +0200

    Stop tracking awesome

--- .config/awesome/activate-rc.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Re-enable a config that was previously disabled through `restore-rc.sh`
-
-mv $HOME/.config/awesome/rc.lua.bad $HOME/.config/awesome/rc.lua
-echo 'awesome.restart()' | awesome-client
--- .config/awesome/restore-rc.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Restore a working awesome environment when something goes wrong.
-# Renames the rc in the config folder and restarts the awesome client.
-# This way, awesome will fall back to `/etc/xdg/awesome/rc.lua`, which is the
-# default configuration.
-
-mv $HOME/.config/awesome/rc.lua $HOME/.config/awesome/rc.lua.bad
-echo 'awesome.restart()' | awesome-client
