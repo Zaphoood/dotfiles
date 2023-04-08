7532f3f40ac392d2e95f11373a83a703f6ed246a
Mathis Weber <mathis.c.weber@web.de>
2023-04-08 16:18:24 +0200

    nvim: load latex template

--- .config/nvim/after/plugin/luasnip.lua
+++ .config/nvim/after/plugin/luasnip.lua
@@ -8,8 +8,8 @@ local func = ls.function_node
 
 cmp.setup({
     sources = {
-        {name = 'nvim_lsp'},
-        {name = 'luasnip'},
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
     },
     mapping = {
         ["<Tab>"] = cmp.mapping(function(fallback)
@@ -22,60 +22,92 @@ cmp.setup({
     }
 })
 
+local function loadTemplate(filename)
+    -- Create `insert` at top of template, so that the cursor stays at the start
+    local template = { insert(0) }
+    local errorMsg = { text("Could not load template: '" .. filename .. "'") }
+
+    local file = io.open(filename, "r")
+    if file == nil then return errorMsg end
+
+    -- Insert first line without leading newline
+    local line = file:read()
+    if line == nil then return errorMsg end
+    table.insert(template, text(line))
+
+    while true do
+        line = file:read()
+        if line == nil then return template end
+        table.insert(template, text({ "", line }))
+    end
+end
+
+local latexTemplate = loadTemplate(
+    vim.api.nvim_call_function('stdpath', { 'config' }) .. "/after/plugin/snippets/template.tex"
+)
+
+
 
 vim.api.nvim_create_autocmd(
-{"Filetype"},
-{
-    pattern = {"tex"},
-    callback = function()
-        ls.add_snippets(nil, {
-            all = {
-                snip(
-                {
-                    trig = "align",
-                    dscr = "Insert an `align*` block"
+    { "Filetype" },
+    {
+        pattern = { "tex" },
+        callback = function()
+            ls.add_snippets(nil, {
+                all = {
+                    snip(
+                        {
+                            trig = "align",
+                            dscr = "Insert an `align*` block"
+                        },
+                        {
+                            text("\\begin{align*}"),
+                            text({ "", "\t" }), insert(0),
+                            text({ "", "\\end{align*}" }),
+                        }
+                    ),
+                    snip(
+                        {
+                            trig = "enumerate",
+                            dscr = "Insert an `enumerate` block"
+                        },
+                        {
+                            text("\\begin{enumerate}"),
+                            text({ "", "\t" }), insert(0),
+                            text({ "", "\\end{enumerate}" }),
+                        }
+                    ),
+                    snip(
+                        {
+                            trig = "proof",
+                            dscr = "Insert a `proof` block"
+                        },
+                        {
+                            text("\\begin{proof}"),
+                            text({ "", "" }), insert(0),
+                            text({ "", "\\end{proof}" }),
+                        }
+                    ),
+                    snip(
+                        {
+                            trig = "block",
+                            dscr = "Insert a `\\begin{...} \\end{...}` block"
+                        },
+                        {
+                            text("\\begin{"), insert(1), text("}"),
+                            text({ "", "\t" }), insert(2),
+                            text({ "", "\\end{" }), func(function(args) return args[1][1] end, { 1 }),
+                            text("}")
+                        }
+                    ),
+                    snip(
+                        {
+                            trig = "template",
+                            dscr = "Insert LaTeX template",
+                        },
+                        latexTemplate
+                    )
                 },
-                {
-                    text("\\begin{align*}"),
-                    text({"", "\t"}), insert(0),
-                    text({"", "\\end{align*}"}),
-                }
-                ),
-                snip(
-                {
-                    trig = "enumerate",
-                    dscr = "Insert an `enumerate` block"
-                },
-                {
-                    text("\\begin{enumerate}"),
-                    text({"", "\t"}), insert(0),
-                    text({"", "\\end{enumerate}"}),
-                }
-                ),
-                snip(
-                {
-                    trig = "proof",
-                    dscr = "Insert a `proof` block"
-                },
-                {
-                    text("\\begin{proof}"),
-                    text({"", ""}), insert(0),
-                    text({"", "\\end{proof}"}),
-                }
-                ),
-                snip(
-                {
-                    trig = "block",
-                    dscr = "Insert a `\\begin{...} \\end{...}` block"
-                },
-                {
-                    text("\\begin{"), insert(1), text("}"),
-                    text({"", "\t"}), insert(2),
-                    text({"", "\\end{"}), func(function(args) return args[1][1] end, {1}),
-                    text("}")
-                }
-                ),
-            },
-        })
-    end
-})
+            })
+        end
+    })
