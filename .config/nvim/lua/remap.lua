6dc1d091280c8cec9428eb18c7f3cab9eda4c846
Mathis Weber <mathis.c.weber@web.de>
2023-06-13 17:32:01 +0200

    nvim: disable `J` and `K`

--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -7,8 +7,6 @@ vim.keymap.set("n", "<leader>ws", function() vim.cmd("set list!") end)
 
 vim.keymap.set("i", "<C-A>", "<Home>", { noremap = true })
 vim.keymap.set("i", "<C-E>", "<End>", { noremap = true })
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "n", "nzz")
 vim.keymap.set("n", "N", "Nzz")
 vim.keymap.set("n", "<leader>n", ":cnext<Enter>zz")
