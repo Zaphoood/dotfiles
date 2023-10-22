9853c47cb6f4da4ae272b03abcab868c77c89266
Mathis Weber <mathis.c.weber@web.de>
2023-10-22 10:13:17 +0200

    nvim: autoclose $ in .tex files

--- .config/nvim/after/plugin/autoclose.lua
+++ .config/nvim/after/plugin/autoclose.lua
@@ -1,5 +1,13 @@
 require("autoclose").setup({
     keys = {
-        ["\""] = { disabled_filetypes = { "tex" } }
+        ["$"] = {
+            close = true,
+            escape = true,
+            pair = "$$",
+            enabled_filetypes = { "tex" },
+        },
+        ["\""] = {
+            disabled_filetypes = { "tex" }
+        }
     }
 })
