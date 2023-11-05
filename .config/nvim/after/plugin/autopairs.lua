afd0f56b4c15ad88782d5d6c811ede35f32e4b1a
Mathis Weber <mathis.c.weber@web.de>
2023-11-05 22:40:17 +0100

    nvim: disable autopairs `'`/`"` for markdown

--- .config/nvim/after/plugin/autopairs.lua
+++ .config/nvim/after/plugin/autopairs.lua
@@ -1,7 +1,8 @@
 local pairs = require("nvim-autopairs")
 local Rule = require("nvim-autopairs.rule")
 
-pairs.get_rules("\"")[1].not_filetypes = { "tex" }
+pairs.get_rules("\"")[1].not_filetypes = { "tex", "markdown" }
+pairs.get_rules("'")[1].not_filetypes = { "markdown" }
 pairs.add_rules({
     Rule("`", "'", "tex"),
 })
