c04bbab4f5e8ce43aafad2347d902a34daeb0ece
Mathis Weber <mathis.c.weber@web.de>
2023-11-11 19:49:40 +0100

    nvim: disable apostrophe autopairs for rust

--- .config/nvim/after/plugin/autopairs.lua
+++ .config/nvim/after/plugin/autopairs.lua
@@ -2,7 +2,7 @@ local pairs = require("nvim-autopairs")
 local Rule = require("nvim-autopairs.rule")
 
 pairs.get_rules("\"")[1].not_filetypes = { "tex", "markdown" }
-pairs.get_rules("'")[1].not_filetypes = { "markdown" }
+pairs.get_rules("'")[1].not_filetypes = { "markdown", "rust" }
 pairs.add_rules({
     Rule("`", "'", "tex"),
 })
